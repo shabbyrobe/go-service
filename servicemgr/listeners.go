09b77856bbbe2dd32775d5aac53f2096ec1e8678
shabbyrobe <code@shabbyrobe.org>
2018-01-31 21:32:50 +0100

    Ender alternative

--- servicemgr/listeners.go
+++ servicemgr/listeners.go
@@ -6,6 +6,12 @@ import service "github.com/shabbyrobe/go-service"
 // unexpectedly.
 //
 // If the failure channel would block, errors are discarded.
+//
+// If you expect a certain number of errors, you should pass at least that
+// number to cap. If not, you may find that any errors that occur between
+// a service stopping unexpectedly and the channel being read from get
+// dropped.
+//
 type FailureListener struct {
 	failures chan error
 }
@@ -42,5 +48,52 @@ func (f *FailureListener) OnServiceEnd(service service.Service, err service.Erro
 		case f.failures <- err:
 		default:
 		}
+	}
+}
+
+// EndListener provides a channel that emits errors when services end
+// unexpectedly, and nil when they end expectedly.
+//
+// If the ends channel would block, items are discarded.
+//
+// If you expect a certain number of ends, you should pass at least that
+// number to cap. If not, you may find that any ends that occur between
+// a service stopping unexpectedly and the channel being read from get
+// dropped.
+//
+type EndListener struct {
+	ends chan error
+}
+
+var _ Listener = &EndListener{}
+
+func NewEndListener(cap int) *EndListener {
+	if cap < 1 {
+		cap = 1
+	}
+	return &EndListener{
+		ends: make(chan error, cap),
+	}
+}
+
+func (e *EndListener) Ends() <-chan error {
+	return e.ends
+}
+
+// SendNonNil sends an arbitrary error through the failure channel if it is not nil.
+// Use it if you want to mix arbitrary goroutine error handling with service failure.
+func (e *EndListener) SendNonNil(err error) {
+	if err != nil {
+		select {
+		case e.ends <- err:
+		default:
+		}
+	}
+}
+
+func (e *EndListener) OnServiceEnd(service service.Service, err service.Error) {
+	select {
+	case e.ends <- err:
+	default:
 	}
 }
